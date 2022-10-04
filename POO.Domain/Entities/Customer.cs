866c0cb572fc6cfd3a9cf1bbecbd772c66f162c3
Gabriel Medeiros <gabrielmedsilva@outlook.com>
2022-10-04 06:14:54 -0300

    feat: Utiliza método construtor para deixar código mais explicado

--- POO.Domain/Entities/Customer.cs
+++ POO.Domain/Entities/Customer.cs
@@ -2,8 +2,20 @@ namespace POO.Domain.Entities
 {
     public class Customer
     {
+        public Customer(string name,  decimal salary)
+        {
+            Name = name;
+            Salary = salary;
+        }
+        public Customer(string name, decimal salary, DateTime? birthDate = null)
+        {
+            Name = name;
+            BirthDate = birthDate;
+            Salary = salary;
+        }
+
         public string Name { get; set; }
-        public DateTime BirthDate { get; set; }
+        public DateTime? BirthDate { get; set; }
         public decimal Salary { get; set; }
     }
 }
--- POO.console/Program.cs
+++ POO.console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using POO.Domain.Entities;
 
 namespace POO.App
 {
@@ -6,6 +7,8 @@ namespace POO.App
     {
         static void Main(string[] args)
         {
+            var cust = new Customer("Gabriel Medeiros", 2000, DateTime.Now);
+
             console.WriteLine("Hello");
         }
     }
